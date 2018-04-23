3326ca877301177c25af13a1e678af50da04d631
bharath0122 <potluri.bharath55555@gmail.com>
2018-04-23 13:03:02 +0200

    Base commit

---
+++ src/VABC.Domain.Prime/Primes.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace VABC.Domain.Prime
+{
+    public static class Primes
+    {
+        public static IList<int> GeneratePrimeNumbers(int upUntil)
+        {
+            bool isPrime = true;
+            var primeList = new List<int>();
+            for (int i = 2; i <= upUntil; i++)
+            {
+                for (int j = 2; j < i; j++)
+                {
+
+                    if (i != j && i % j == 0)
+                    {
+                        isPrime = false;
+
+                        break;
+                    }
+
+                }
+                if (isPrime)
+                {
+
+                    primeList.Add(i);
+
+                }
+
+
+                isPrime = true;
+            }
+            return primeList;
+        }
+    }
+}
---
+++ src/VABC.Tutorials.PrimeExe/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using VABC.Domain.Prime;
+
+namespace VABC.Tutorials.Prime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = Primes.GeneratePrimeNumbers(1000);
+            foreach (var prime in list)
+            {
+                Console.WriteLine(prime);
+            }
+        }
+    }
+}
